6be432f1d2a22530b7f65346f152fbd78d927ccc
Dan Kuykendall <dan@kuykendall.org>
2025-03-05 12:29:35 -0800

    Added buffer plugins

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN chmod +x ixr-buildall.sh
 COPY src ./src
 
 RUN npm install
+RUN npm install process --save-dev
 
 # Command to run the application.
 ENTRYPOINT [ "./ixr-buildall.sh" ]
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,50 @@
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports =
-{
+module.exports = {
 	mode: "development",
 	devtool: "inline-source-map",
-	entry:
-	{
-		main: "./src/test.ts",
+	entry: {
+		main: "./src/test.ts"
 	},
-	output:
-	{
+	output: {
 		path: path.resolve(__dirname, './dist'),
 		filename: "iXRLibForWebXR.js" // <--- Will be compiled to this single file
 	},
-	resolve:
-	{
+	resolve: {
 		extensions: [".ts", ".tsx", ".js"],
 		fallback: {
-            "fs": false,
-            "tls": false,
-            "net": false,
-            "path": false,
-            "zlib": false,
-            "http": false,
-            "https": false,
-            "stream": require.resolve('stream-browserify'),
-            "crypto": require.resolve('crypto-browserify'),
-            "buffer": require.resolve('buffer/'),
-            "util": require.resolve('util/'),
-			"vm": require.resolve('vm-browserify')
-        }
+			fs: false,
+			tls: false,
+			net: false,
+			path: false,
+			zlib: false,
+			http: false,
+			https: false,
+			stream: require.resolve('stream-browserify'),
+			crypto: require.resolve('crypto-browserify'),
+			buffer: require.resolve('buffer/'),
+			util: require.resolve('util/'),
+			vm: require.resolve('vm-browserify'),
+			process: require.resolve('process/browser')
+		}
 	},
-	module:
-	{
-		rules:
-		[
+	module: {
+		rules: [
 			{
 				test: /\.tsx?$/,
 				loader: "ts-loader"
 			}
 		]
-	}
+	},
+	plugins: [
+		// Work around for Buffer is undefined:
+		// https://github.com/webpack/changelog-v5/issues/10
+		new webpack.ProvidePlugin({
+			Buffer: ['buffer', 'Buffer'],
+		}),
+		new webpack.ProvidePlugin({
+			process: 'process/browser',
+		}),
+	],
 };
